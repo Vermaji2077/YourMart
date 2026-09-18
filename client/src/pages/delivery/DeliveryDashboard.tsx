44f45d46c6dd52ea33847cf70192566c81faa254
Vermaji2077 <nishantv181@gmail.com>
2026-09-18 14:30:20 +0530

    fial1

--- client/src/pages/delivery/DeliveryDashboard.tsx
+++ client/src/pages/delivery/DeliveryDashboard.tsx
@@ -11,7 +11,7 @@ import toast from "react-hot-toast";
 /**
  */
 
-const API_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000/api"
+const API_URL = import.meta.env.VITE_API_URL 
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("delivery_token");
